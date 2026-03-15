ba335330188288425bf8081a23a32a19a9c5ccb2
Gurupreet <kumargurupreet2008@gmail.com>
2026-03-16 01:04:20 +0530

    second commit

--- ATLAS_API/app/telegram_api.py
+++ ATLAS_API/app/telegram_api.py
@@ -19,10 +19,6 @@ async def webhook(request: Request):
 
     # extracting chat id
 
-    """
-    {'update_id': 72763059, 'message': {'message_id': 118, 'from': {'id': 8779748119, 'is_bot': False, 'first_name': 'Gurupreet', 'username': 'GurupreetKumar', 'language_code': 'en'}, 'chat': {'id': 8779748119, 'first_name': 'Gurupreet', 'username': 'GurupreetKumar', 'type': 'private'}, 'date': 1773601359, 'text': 'dgrf'}}
-
-    """
     chat_id=data['message']['chat']['id']
     # print(chat_id)
 
