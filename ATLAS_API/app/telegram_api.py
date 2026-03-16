4d9ac769e00675fa3467b5bfcdfa0af77d122ad3
Gurupreet <kumargurupreet2008@gmail.com>
2026-03-16 13:44:36 +0530

    added image and video saving facility to local.

--- ATLAS_API/app/telegram_api.py
+++ ATLAS_API/app/telegram_api.py
@@ -1,32 +1,65 @@
 from fastapi import APIRouter, Request
-import requests
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-BOT_TOKEN=os.getenv('TELEGRAM_BOT_API_TOKEN')
-
-URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+from ATLAS_API.app.utilities.utilities import send_message, get_file_path
+from google import genai
 
 router = APIRouter(prefix="/telegram", tags=["telegram"])
 
 @router.post('/webhook')
 async def webhook(request: Request):
 
+
     data = await request.json()
     print(data)
 
     # extracting chat id
+    try:
 
-    chat_id=data['message']['chat']['id']
-    # print(chat_id)
+        chat_id=data['message']['chat']['id']
+        text = data['message'].get('text')
 
-    send = requests.post(URL, json={
-        "chat_id": chat_id,
-        "text": "Boss is busy right now!"
-    })
+        # checking that is message in the field or not?
+        if data['message']:
 
-    print(send.json())
+            # Checking that the sticker is sent or not if sent, then no action
+            # It is also checking for the animation
+            if data['message'].get('sticker') or data['message'].get('animation'):
+
+                send_message(chat_id=chat_id, text="Don't send me sticker/GIF! "
+                                                   "I will not tell you again.")
+
+            elif data['message'].get('photo'):
+                # send_message(chat_id=chat_id, text="Yeah! I got your photo")
+                # extracting file_id
+                """
+                NOTE: In response there will be many file_id, this is of the different quality of the image from low to high so taking the last item of list.
+                """
+                file_id = data['message']['photo'][-1].get('file_id')
+
+                # extracting caption if available
+                caption = data['message'].get('caption')
+
+                get_file_path(file_id, "photo", caption, chat_id)
+
+            elif data['message'].get('video'):
+                # send_message(chat_id=chat_id, text="Yeah! I got your video")
+                file_id = data['message']['video'].get('file_id')
+                caption = data['message'].get('caption')
+                get_file_path(file_id, "video", caption, chat_id)
+
+            else:
+                # This client will auto fetch the gemini api key named GEMINI_API_KEY from environment
+                # client = genai.Client()
+                #
+                # response = client.models.generate_content(
+                #     model="gemini-3-flash-preview", contents=f"{text}"
+                # )
+                # # print(response)
+                # send_message(chat_id=chat_id, text=response.text)
+                send_message(chat_id=chat_id, text="wait for few hours")
+    except Exception as e:
+        print(e)
+
 
     return {"ok": True}
+
+
---
+++ ATLAS_API/app/utilities/utilities.py
@@ -0,0 +1,71 @@
+# Here I will create the utilities function.
+import requests
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+"""
+base dir making using pathlib library of the python.
+This gives the url where it is written. Means when it is written in utilities then it will will from base to here path.
+
+.parent is equivalent of .. which take to the parent directory
+"""
+
+load_dotenv()
+
+BOT_TOKEN=os.getenv('TELEGRAM_BOT_API_TOKEN')
+
+URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+# This function is for text to send the message to the telegram
+def send_message(chat_id, text):
+    requests.post(URL, json={
+        "chat_id": chat_id,
+        "text": text
+    })
+
+# when files or photo or docs come, telegram send the file_id, from which we will extract file path and then download the file from file path.
+
+"""
+This is the response for photo:
+
+{'ok': True, 'result': {'file_id': 'AgACAgUAAxkBAAPgabecwzZlzKj3i1wLUfjJwUu9w-YAAjsNaxvWGcBVInVLeSotz3cBAAMCAAN5AAM6BA', 'file_unique_id': 'AQADOw1rG9YZwFV-', 'file_size': 218079, 'file_path': 'photos/file_0.jpg'}}
+"""
+def get_file_path(file_id, file_type:str, caption, chat_id):
+    file_path_url = F'https://api.telegram.org/bot{BOT_TOKEN}/getFile?file_id={file_id}'
+
+    response = requests.get(file_path_url)
+    file = response.json()
+    file_path = file['result']['file_path']
+
+    get_file_binary(file_path, file_type, caption, chat_id)
+
+# this function is for getting the raw file binary:
+def get_file_binary(file_path, file_type:str, caption, chat_id):
+    file_get_binary_url = f'https://api.telegram.org/file/bot{BOT_TOKEN}/{file_path}'
+
+    response = requests.get(file_get_binary_url)
+    # print(response.content) here the binary data will be returned
+
+    # writing the binary into the file
+    if file_type == 'photo':
+        image_path = BASE_DIR/'telegram_files'/'photos'
+        if caption is not None:
+            # print(os.getcwd())
+            with open(f'{image_path}/{caption}.jpg', 'wb') as f:
+                f.write(response.content)
+
+            send_message(chat_id, f"Your images has been saved successfully with name {caption}.jpg")
+
+        else:
+            send_message(chat_id, "send the image with caption, as that name will be used to save the image. Use underscore instead of space.")
+    if file_type == 'video':
+        video_path = BASE_DIR/'telegram_files'/'videos'
+        if caption is not None:
+            with open(f'{video_path}/{caption}.mp4', 'wb') as f:
+                f.write(response.content)
+
+            send_message(chat_id, f"Your videos has been saved successfully with name {caption}.mp4")
+        else:
+            send_message(chat_id, "send the video with caption, as that name will be used to save the video. Use underscore instead of space.")
